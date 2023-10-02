4ae4325cfb810ee95fc5aa33885e191408e2758b
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-10-02 22:11:39 +0200

    Added jwt

---
+++ verifyJWT/index.mjs
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+
+export const handler = async (event) => {
+
+    let response = {};
+
+    try {
+        let token = event.headers.authorization;
+
+        if (!token) {
+            response.statusCode = 401;
+            response.body = JSON.stringify({ message: 'No token provided.' });
+            return response;
+        }
+        token = token.replace('Bearer ', '');
+
+        const decodedToken = jwt.verify(token, '123abc');
+
+        console.log('Decoded token:', decodedToken);
+
+        response.statusCode = 200;
+        response.body = JSON.stringify({ message: 'Token is valid', decodedToken });
+        return response;
+
+    } catch (error) {
+        console.error('Token verification failed:', error);
+
+        response.statusCode = 401;
+        response.body = JSON.stringify({ message: 'Token is invalid' });
+        return response;
+    }
+};
